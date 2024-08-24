8da55639b55f5d6b4974909ca4391341f6124d77
Sanid-Assara <sanid.assara@gmail.com>
2024-08-24 09:41:19 +0200

    connect the  profile api

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,27 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Profile() {
-  axios
-    .get("http://localhost:8080/API/users/profile ", {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    })
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-    .finally(() => {});
+  const [user, setUser] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/API/users/profile ", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data.userId);
+        axios
+          .get(`http://localhost:8080/API/users/${res.data.userId}`)
+          .then((res) => {
+            console.log(res.data);
+            setUser(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {});
+  }, []);
+
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-red-600">Profile</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-red-600">Email: {user.email}</h1>
+        <h1 className="text-4xl font-bold text-red-600">
+          Name: {user.firstname}
+        </h1>
+        <h1 className="text-4xl font-bold text-red-600">
+          Saved Recipes : {user.savedRecipes}
+        </h1>
+        <br />
       </div>
+      savedRecipes
     </>
   );
 }

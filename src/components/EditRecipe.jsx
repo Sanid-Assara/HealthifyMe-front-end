7f6afc629693d3c127565a2365e2ada5eee552e0
Sanid-Assara <sanid.assara@gmail.com>
2024-08-30 10:28:33 +0200

    Create Edit and Delete Ricpe components

---
+++ src/components/EditRecipe.jsx
@@ -0,0 +1,7 @@
+export default function EditRecipe() {
+  return (
+    <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+      Edit
+    </button>
+  );
+}
